bc7c246c30f9f138f2235acec567c0cf24609b0c
wskong <wskong@ntels.com>
2022-01-19 18:41:26 +0900

    22/01/19

--- src/main/java/com/wskong/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/wskong/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.wskong.book.springboot.service.posts;
 
 import com.wskong.book.springboot.domain.Posts.Posts;
 import com.wskong.book.springboot.domain.Posts.PostsRepository;
+import com.wskong.book.springboot.web.dto.PostsListResponseDto;
 import com.wskong.book.springboot.web.dto.PostsResponseDto;
 import com.wskong.book.springboot.web.dto.PostsSaveRequestDto;
 import com.wskong.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,10 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto:: new).collect(Collectors.toList());
     }
 }
--- src/main/java/com/wskong/book/springboot/web/IndexController.java
+++ src/main/java/com/wskong/book/springboot/web/IndexController.java
@@ -1,21 +1,26 @@
 package com.wskong.book.springboot.web;
 
+import com.wskong.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
-
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 
 }
